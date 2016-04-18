dd9761cc4596332fafd63fdb68f172a75c36fbf6
Micke Larsson <mikael.larsson@laget.se>
2016-04-18 16:03:57 +0200

    Push done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { getConfig } from './confighelpers';
 import { extractMessagesFromGlob, toPot } from 'react-gettext-parser';
 import { fetchUrl } from 'fetch';
+import { mergePotContents } from '@lagetse/pot-merge';
 
 const pull = () => {
   // pull trans from transifex
@@ -17,21 +18,49 @@ const push = () => {
   const conf = getConfig();
   const messages = extractMessagesFromGlob(conf.extract.source);
   const pot = toPot(messages);
-  // console.log(pot);
 
   const { project, resource, username, password } = conf.transifex;
+
   // pull translations from transifex
   fetchUrl(`http://www.transifex.com/api/2/project/${project}/resource/${resource}/content?file`, {
     headers: { Authorization: `Basic ${new Buffer(`${username}:${password}`).toString('base64')}` },
   }, (err, meta, body) => {
-    console.log(meta);
-    console.log(err);
-    console.log(body.toString('utf-8'));
-  });
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
 
-  // merge pots
-  // push pots to transifex
-  console.log('push push');
+    if (meta.status >= 400) {
+      console.log(meta);
+      process.exit(1);
+    }
+
+    const data = body.toString('utf-8');
+    // merge pots
+    const output = mergePotContents(pot, data);
+
+    // push pots to transifex
+    fetchUrl(`http://www.transifex.com/api/2/project/${project}/resource/${resource}/content/`, {
+      method: 'PUT',
+      payload: JSON.stringify({ content: output }),
+      headers: {
+        'Authorization': `Basic ${new Buffer(`${username}:${password}`).toString('base64')}`,
+        'Content-Type': 'application/json',
+      },
+    }, (err, meta, body) => {
+      if (err) {
+        console.log(err);
+        process.exit(1);
+      }
+
+      if (meta.status >= 400) {
+        console.log(meta);
+        process.exit(1);
+      }
+
+      console.log('Source file updated and uploaded.');
+    });
+  });
 };
 
 export { pull, push };
